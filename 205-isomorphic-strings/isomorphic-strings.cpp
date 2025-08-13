8568c54e1fb4dded350196a6475defb4f4a7f01c
Navneet Mishra <151377657+Navimish@users.noreply.github.com>
2025-08-13 17:09:38 +0530

    Time: 6 ms (9.81%) | Memory: 9.5 MB (29.94%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -5,25 +5,17 @@ public:
         unordered_map<char,char> mp1;
         unordered_map<char,char> mp2;
 
-        for(int i =0 ; i<s.length(); i++){
+        for(int i = 0; i<s.length(); i++){
 
-            char ch1 = s[i];
-            char ch2 = t[i];
+            if((mp1.count(s[i]) && mp1[s[i]] != t[i] ) || (mp2.count(t[i]) && mp2[t[i]] != s[i])) return false;
 
-            if((mp1.find(ch1) != mp1.end() && mp1[ch1] != ch2 )||( mp2.find(ch2) != mp2.end () && mp2[ch2] != ch1) ){
-                return false;
-
-            }
-
-            mp1[ch1] = ch2;
-            mp2[ch2] = ch1;
+             mp1[s[i]] = t[i];
+            mp2[t[i]] = s[i];
         }
 
-        return true;
-
-        
-
-        
+       
+    return true;
         
     }
+
 };
