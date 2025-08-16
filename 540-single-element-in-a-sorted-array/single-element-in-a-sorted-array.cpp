315ae975e2dcf86cd9481135e954cfe65a050391
Navneet Mishra <151377657+Navimish@users.noreply.github.com>
2025-08-16 15:22:48 +0530

    Time: 0 ms (100.00%) | Memory: 26.1 MB (67.49%) - LeetSync

---
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        
+        int n = nums.size();
+        int l = 0;
+        int h = n-1;
+
+        bool iseven;
+
+  
+
+        while(l<h){
+
+                  int mid = l+(h-l)/2;
+
+                if((h-mid)%2 == 0){
+                    iseven = true;
+                }else{
+                    iseven = false;
+                }
+
+            if(nums[mid] == nums[mid+1]){
+
+                if(iseven){
+                    l = mid+2;
+                }else{
+                    h = mid-1;
+                }
+
+            }else{
+
+                if(iseven){
+                    h = mid;
+                }else{
+                    l = mid+1;
+                }
+
+            }
+        }
+
+        return nums[l];
+
+
+
+
+   
+        
+    }
+};
