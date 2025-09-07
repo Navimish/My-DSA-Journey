df917fb76256a3bba3079640b9855949d77528a2
Navneet Mishra <151377657+Navimish@users.noreply.github.com>
2025-09-08 01:33:23 +0530

    Time: 3 ms (61.59%) | Memory: 23.4 MB (58.13%) - LeetSync

---
+++ 155-min-stack/min-stack.cpp
@@ -0,0 +1,53 @@
+class MinStack {
+        stack<int> st;
+        stack<int> minst;
+        int minimum = INT_MAX;
+public:
+    MinStack() {
+
+   
+        
+    }
+    
+    void push(int val) {
+        st.push(val);
+        minimum = minst.empty()?val: min(val,minst.top());
+        minst.push(minimum);
+
+    
+        
+    }
+    
+    void pop() {
+
+        st.pop();
+        minst.pop();
+        if(!minst.empty()){
+            minimum = minst.top();
+        }else{
+            minimum = INT_MAX;
+        }
+        
+    }
+    
+    int top() {
+
+       return st.top();
+        
+    }
+    
+    int getMin() {
+
+       return minst.top();
+        
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
