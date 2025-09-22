58ba7dfb21f0fc179eea6b963a2b3bb3a3ae19d1
Navneet Mishra <151377657+Navimish@users.noreply.github.com>
2025-09-22 15:49:39 +0530

    Time: 5 ms (52.32%) | Memory: 10.1 MB (42.44%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -5,21 +5,35 @@ public:
         int n = s1.size();
         int m = s2.size();
 
-        int l = 0;
+        vector<int> freq_s1(26,0);
+        vector<int> freq_s2(26,0);
 
-        sort(s1.begin(),s1.end());
-
-        for(int r =0; r<m-n+1; r++ ){
-
-            string sub = s2.substr(r,n);
-
-            sort(sub.begin(), sub.end());
-
-            if(sub == s1) return true;
+        for(auto &c: s1){
+            freq_s1[c-'a']++;
         }
 
+        int l =0;
+        int r =0;
+
+        while(r<m){
+            freq_s2[s2[r]-'a']++;
+
+            if(r-l+1 > n){
+                freq_s2[s2[l]-'a']--;
+                l++;
+
+            }
+
+
+            if(freq_s1 == freq_s2) return true;
+
+            r++;
+            
+        }
 
         return false;
+
+      
         
     }
 };
