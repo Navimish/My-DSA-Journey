05a299a5b0fad0bda820bbcfa48d6a5cdec6d9ff
Navneet Mishra <151377657+Navimish@users.noreply.github.com>
2026-01-28 19:39:41 +0530

    Time: 0 ms (100.00%) | Memory: 15.5 MB (87.13%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.cpp
+++ 328-odd-even-linked-list/odd-even-linked-list.cpp
@@ -20,24 +20,20 @@ public:
 
         while(even && even->next){
 
-            odd->next= even->next;
+            odd->next = even->next;
             odd = odd->next;
 
             even->next = odd->next;
-
             even = even->next;
 
 
 
-            
         }
 
         odd->next = evenhead;
-
         return head;
 
 
-   
         
     }
 };
