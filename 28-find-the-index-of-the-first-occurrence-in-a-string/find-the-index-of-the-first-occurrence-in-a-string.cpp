9c20788baa24894e178ae188768b8b7feedb1e70
Navneet Mishra <151377657+Navimish@users.noreply.github.com>
2026-02-06 20:13:57 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (72.41%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -7,9 +7,13 @@ public:
 
         for(int i = 0; i<m-n+1; i++){
 
-            if(needle == haystack.substr(i,n)){
-                return i;
-            }
+           int j =0;
+
+           while(j<n && haystack[i+j] == needle[j]){
+            j++;
+           }
+
+           if(j == n) return i;
         }
 
         return -1;
