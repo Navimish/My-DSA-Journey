2c8724deb6271c455d501a2cfc7a2cd073c8d502
Navneet Mishra <151377657+Navimish@users.noreply.github.com>
2026-04-12 20:09:22 +0530

    Time: 22 ms (19.54%) | Memory: 14.4 MB (21.61%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -2,23 +2,25 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
 
-        unordered_set<int> set;
-        int l = 0;
+        unordered_set<int> st;
+
+        int l =0;
         int len = 0;
 
-        for(int r = 0; r<s.length(); r++ ){
+        for(int r = 0; r<s.length(); r++){
 
-            while(set.find(s[r]) != set.end()){
-                set.erase(s[l]);
+            while(st.find(s[r]) != st.end()){
+                st.erase(s[l]);
                 l++;
             }
 
-            set.insert(s[r]);
-            len = max(len, r-l+1);
-
+            st.insert(s[r]);
+            len = max(len,r-l+1);
         }
 
         return len;
+
+
         
     }
 };
