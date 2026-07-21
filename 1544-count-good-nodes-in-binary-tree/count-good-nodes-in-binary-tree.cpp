8a15e82a609cad0f071451a51b63018f4ac8c35d
Navneet Mishra <151377657+Navimish@users.noreply.github.com>
2026-07-21 23:08:20 +0530

    Time: 83 ms (95.68%) | Memory: 88.2 MB (85.15%) - LeetSync

--- 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.cpp
+++ 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.cpp
@@ -6,33 +6,37 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
 
-    void cgn(TreeNode* root,int& count,int maxval){
-        if(!root) return;
+    void cgn(TreeNode* root, int& count, int maxval){
 
-        if(root->val>=maxval){
+        if(!root) return ;
+
+        if(root->val>= maxval){
             count++;
             maxval = root->val;
         }
 
-        cgn(root->left,count,maxval);
-        cgn(root->right,count,maxval);
+        cgn(root->left,count, maxval);
+        cgn(root->right,count, maxval);
+
 
     }
+
 
     int goodNodes(TreeNode* root) {
 
         int count = 0;
-        if (!root) return count;
+
+        if(!root) return  count;
 
         cgn(root,count,root->val);
 
         return count;
+        
     }
 };
