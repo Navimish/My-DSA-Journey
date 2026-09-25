3d22b5f45b0888fa05fee133f63a229b82acf4d3
Navneet Mishra <151377657+Navimish@users.noreply.github.com>
2026-09-25 15:42:02 +0530

    Time: 0 ms (100.00%) | Memory: 21 MB (60.49%) - LeetSync

---
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -0,0 +1,68 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+
+        int m = matrix.size();
+        int n = matrix[0].size();
+        bool firstColImpacted = false;
+        bool firstRowImpacted = false;
+
+        // firstly check if first row is impacted or not
+
+        for(int col = 0; col < n; col++){
+
+            if(matrix[0][col] == 0){
+                firstRowImpacted = true;
+                break;
+            }
+        }
+        // firstly check if first col is impacted or not
+
+        for(int row = 0; row < m; row++){
+
+            if(matrix[row][0] == 0){
+                firstColImpacted = true;
+                break;
+            }
+        }
+
+        // set markers
+
+        for(int i = 1; i<m ; i++){
+            for(int j = 1; j< n; j++){
+                if(matrix[i][j] == 0){
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+                }
+
+
+            }
+        }
+        for(int i = 1; i<m ; i++){
+            for(int j = 1; j< n; j++){
+                if(matrix[0][j] == 0 || matrix[i][0] == 0){
+                   matrix[i][j] = 0;
+                }
+
+
+            }
+        }
+
+        if(firstRowImpacted){
+            for(int j =0; j < n; j++){
+                matrix[0][j] = 0;
+            }
+        }
+        if(firstColImpacted){
+            for(int i =0; i < m; i++){
+                matrix[i][0] = 0;
+            }
+        }
+
+
+
+
+
+        
+    }
+};
